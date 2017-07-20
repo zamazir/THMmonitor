9aea9cdd741482334a09f07337e767c85223f0a6
Amazigh Zerzour <amazigh.zerzour@gmail.com>
2017-07-20 09:32:40 +0200

    Read binary files. Implement more reliable sensor<-->subsystem mapping.

---
+++ mapping.py
@@ -0,0 +1,119 @@
+mapping = {
+    'Payload Temperature OW1':
+        {'subsystem': 'Payload',
+         'component': 'Toppanel'},
+    'Payload Temperature OW2':
+        {'subsystem': 'Payload',
+         'component': 'Toppanel'},
+    'Payload Temperature OW3':
+        {'subsystem': 'Payload',
+         'component': 'Toppanel'},
+    'Payload Temperature OW4':
+        {'subsystem': 'Payload',
+         'component': 'Toppanel'},
+    'Payload Temperature OW5':
+        {'subsystem': 'Payload',
+         'component': 'Toppanel'},
+    'Payload Temperature OW6':
+        {'subsystem': 'Payload',
+         'component': 'Toppanel'},
+    'ADCS Sidepanel X+ Temperature BMX':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X+'},
+    'ADCS Sidepanel X+ Temperature OW1':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X+'},
+    'ADCS Sidepanel X+ Temperature OW2':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X+'},
+    'ADCS Sidepanel X+ Temperature OW3':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X+'},
+    'ADCS Sidepanel X- Temperature BMX':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X-'},
+    'ADCS Sidepanel X- Temperature OW1':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X-'},
+    'ADCS Sidepanel X- Temperature OW2':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X-'},
+    'ADCS Sidepanel X- Temperature OW3':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel X-'},
+    'ADCS Sidepanel Y+ Temperature BMX':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y+'},
+    'ADCS Sidepanel Y+ Temperature OW1':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y+'},
+    'ADCS Sidepanel Y+ Temperature OW2':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y+'},
+    'ADCS Sidepanel Y+ Temperature OW3':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y+'},
+    'ADCS Sidepanel Y- Temperature BMX':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y-'},
+    'ADCS Sidepanel Y- Temperature OW1':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y-'},
+    'ADCS Sidepanel Y- Temperature OW2':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y-'},
+    'ADCS Sidepanel Y- Temperature OW3':
+        {'subsystem': 'ADCS',
+         'component': 'Sidepanel Y-'},
+    'ADCS Sidepanel Z+ Temperature OW1':
+        {'subsystem': 'ADCS',
+         'component': 'Toppanel'},
+    'ADCS Sidepanel Z+ Temperature OW2':
+        {'subsystem': 'ADCS',
+         'component': 'Toppanel'},
+    'ADCS Sidepanel Z+ Temperature OW3':
+        {'subsystem': 'ADCS',
+         'component': 'Toppanel'},
+    'ADCS Sidepanel Z+ Temperature OW4':
+        {'subsystem': 'ADCS',
+         'component': 'Toppanel'},
+    'ADCS Mainpanel Z- Temperature BMX':
+        {'subsystem': 'ADCS',
+         'component': 'Mainpanel'},
+    'ADCS Mainpanel Z- Temperature OW1':
+        {'subsystem': 'ADCS',
+         'component': 'Mainpanel'},
+    'EPS Board Temperature':
+        {'subsystem': 'EPS',
+         'component': 'EPS Board'},
+    'EPS Battery Board Temperature':
+        {'subsystem': 'EPS',
+         'component': 'EPS Board'},
+    'EPS Battery Temperature 1':
+        {'subsystem': 'EPS',
+         'component': 'Battery'},
+    'EPS Battery Temperature 2':
+        {'subsystem': 'EPS',
+         'component': 'Battery'},
+    'COM S-Band Temperature 1':
+        {'subsystem': 'COM',
+         'component': 'S-Band'},
+    'COM S-Band Temperature 2':
+        {'subsystem': 'COM',
+         'component': 'S-Band'},
+    'COM S-Band Temperature 3':
+        {'subsystem': 'COM',
+         'component': 'S-Band'},
+    'COM UHF-VHF Temperature 1':
+        {'subsystem': 'COM',
+         'component': 'UHF-VHF'},
+    'COM UHF-VHF Temperature 2':
+        {'subsystem': 'COM',
+         'component': 'UHF-VHF'},
+    'COM UHF-VHF Temperature 3':
+        {'subsystem': 'COM',
+         'component': 'UHF-VHF'},
+    'CDH Temperature 1':
+        {'subsystem': 'CDH',
+         'component': 'CDH'},
+}
